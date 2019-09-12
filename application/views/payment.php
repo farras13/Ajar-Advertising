5d330374ae67282805af76ee95ff047246ebb938
adit <aeka489@gmail.com>
2019-09-12 22:11:15 +0700

    add layout payment

--- application/views/payment.php
+++ application/views/payment.php
@@ -1,141 +1,88 @@
+<div data-include="header"> <?php include 'include/header.php'; ?> </div>
 
-        <div data-include="header"> <?php include 'include/header.php'; ?> </div>
+<div class="col-xl-12 p-0 m-0 w-100">
+	<div class="row w-100 m-0">
+		<div class="col-md-4 m-0 p-0 ">
 
-        <div class="col-xl-12 p-0 m-0 w-100">
-            <div class="row w-100 m-0">
-                <div class="col-md-4 m-0 p-0 ">
+			<div id="demo" class="carousel slide payment" data-ride="carousel">
+				<!-- Indicators -->
+				<ul class="carousel-indicators ">
+					<li data-target="#demo" data-slide-to="0" class="active"></li>
+					<li data-target="#demo" data-slide-to="1"></li>
+					<li data-target="#demo" data-slide-to="2"></li>
+				</ul>
 
-                    <div id="demo" class="carousel slide payment" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ul class="carousel-indicators ">
-                            <li data-target="#demo" data-slide-to="0" class="active"></li>
-                            <li data-target="#demo" data-slide-to="1"></li>
-                            <li data-target="#demo" data-slide-to="2"></li>
-                        </ul>
+				<!-- The slideshow -->
+				<div class="carousel-inner h-50">
+					<div class="carousel-item active h-75">
+						<p>Lorem Ipsum adalah contoh teks atau
+							dummy dalam industri percetakan dan
+							penataan huruf atau typesetting</p>
+					</div>
+					<div class="carousel-item  h-75">
+						<p>Lorem Ipsum adalah contoh teks atau
+							dummy dalam industri percetakan dan
+							penataan huruf atau typesetting</p>
+					</div>
+					<div class="carousel-item h-75">
+						<p>Lorem Ipsum adalah contoh teks atau
+							dummy dalam industri percetakan dan
+							penataan huruf atau typesetting</p>
+					</div>
+				</div>
 
-                        <!-- The slideshow -->
-                        <div class="carousel-inner h-50">
-                            <div class="carousel-item active h-75">
-                                <p>Lorem Ipsum adalah contoh teks atau
-                                    dummy dalam industri percetakan dan
-                                    penataan huruf atau typesetting</p>
-                            </div>
-                            <div class="carousel-item  h-75">
-                                <p>Lorem Ipsum adalah contoh teks atau
-                                    dummy dalam industri percetakan dan
-                                    penataan huruf atau typesetting</p>
-                            </div>
-                            <div class="carousel-item h-75">
-                                <p>Lorem Ipsum adalah contoh teks atau
-                                    dummy dalam industri percetakan dan
-                                    penataan huruf atau typesetting</p>
-                            </div>
-                        </div>
+				<!-- Left and right controls -->
+				<a class="carousel-control-prev" href="#demo" data-slide="prev">
+					<span class="carousel-control-prev-icon"></span>
+				</a>
+				<a class="carousel-control-next" href="#demo" data-slide="next">
+					<span class="carousel-control-next-icon"></span>
+				</a>
+			</div>
 
-                        <!-- Left and right controls -->
-                        <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                            <span class="carousel-control-prev-icon"></span>
-                        </a>
-                        <a class="carousel-control-next" href="#demo" data-slide="next">
-                            <span class="carousel-control-next-icon"></span>
-                        </a>
-                    </div>
+		</div>
+		<div class="col-md-8 pt-5 bg-white">
+			<div class="container px-5">
+				<div class="text-center">
+					<h3>LET’S FINISH POWERING YOU UP !</h3>
+					<p>This Package is Right for You</p>
+				</div>
 
-                </div>
-                <div class="col-md-8 pt-5 bg-white">
-                    <div class="container px-5">
-                        <div class="text-center">
-                            <h3>LET’S FINISH POWERING YOU UP !</h3>
-                            <p>This Package is Right for You</p>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-7 pl-4">
-                                <table class="payment-table">
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <p>Name of Product </p>
-                                                <p>Price / Unit </p>
-                                                <p>Total Unit </p>
-                                                <p>Total Price</p>
-                                                <p>Discount</p>
-                                                <p>Tax (10%)</p>
-                                                <p>Total Payment</p>
-                                            </td>
-                                            <td class="pl-5 payment-value font-weight-bold">
-                                                <p>: Package A </p>
-                                                <p>: IDR 1.000.000</p>
-                                                <p>: 1 </p>
-                                                <p>: IDR 1.000.000</p>
-                                                <p>: 0</p>
-                                                <p>: IDR 100.000</p>
-                                                <p>: IDR 1.100.000</p>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+				<hr>
 
-                            </div>
-                            <div class="col-md-5 payment-radio">
-                                <p>Select Your Payment :</p>
-                                <p>
-                                    <span class="input-group-addon">
-                                        <input name="payment" type="radio">
-                                        <img class="radio-img"
-                                            src="https://res.cloudinary.com/dkswpljtg/image/upload/v1531301303/Mastercard-Visa-Limited-Acceptance-Programs_uvpqvf.png">
-                                    </span>
-                                </p>
-                                <p>
-                                    <span class="input-group-addon">
-                                        <input name="payment" type="radio">
-                                        <img class="radio-img"
-                                            src="http://mnctrijaya.com/uploads/news/bank_mandiri.jpg">
-                                    </span>
-                                </p>
-                                <p>
-                                    <span class="input-group-addon">
-                                        <input name="payment" type="radio">
-                                        <img class="radio-img"
-                                            src="http://chandra.my.id/wp-content/uploads/2012/07/logo-bank-bca.jpg">
-                                    </span>
-                                </p>
-                                <p>
-                                    <span class="input-group-addon">
-                                        <input name="payment" type="radio">
-                                        <img class="radio-img"
-                                            src="https://4.bp.blogspot.com/-WPYTKgFutro/WEPx1YEAvXI/AAAAAAAABqg/_X_qPtfzTJYCaBSbzDFGK1z7TpkGAx5LQCLcB/s1600/Cara%2BDaftar%2BE%2BPay%2BBRI%2Bdan%2BKeunggulannya%252Clogin%2Bepay%2Bbri%252Cepay%2Bbri%2Btokopedia%252Ct%2Bbank%2Bbri%252Cepay%2Bmandiri%252Cbiaya%2Be-pay%2Bbri%252Ce%2Bkatalog%2Bbri%252Cmocash%2Bbri%252Ce%2Bcatalog%2Bbri%252C.jpg">
-                                    </span>
-                                </p>
-                                <p>
-                                    <span class="input-group-addon">
-                                        <input name="payment" type="radio">
-                                        <img class="radio-img"
-                                            src="http://3.bp.blogspot.com/-DqRReIuCgcc/UjnSRcKPIgI/AAAAAAAAAJI/66Bwh6-3IIo/s1600/CimbClicks.png">
-                                    </span>
-                                </p>
-                                <p>
-                                    <span class="input-group-addon">
-                                        <input name="payment" type="radio">
-                                        <img class="radio-img"
-                                            src="https://res.cloudinary.com/dkswpljtg/image/upload/v1531301303/Mastercard-Visa-Limited-Acceptance-Programs_uvpqvf.png">
-                                    </span>
-                                </p>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="col-md-12 w-100 text-right mt-4">
-                            <button class=" btn btn-abbys px-5 py-2 font-weight-bold">PAY NOW</button>
-                        </div>
-                        <div class="col-md-12 mt-3">
-                            <p>(*) Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                Ipsum is
-                                simply dummy text of the printing
-                                and typesetting industry.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
 
-       <div data-include="footer-2"> <?php include 'include/footer.php'; ?> </div>
+
+				<table class="table table-bordered">
+					<tr>
+						<td class="w-50">
+							<div class="row">
+								<div class="col-lg-2 py-2 my-auto text-center">
+									<img height="30" width="50"
+										src="https://image.flaticon.com/icons/svg/1899/1899722.svg" alt="">
+								</div>
+								<div class="col-lg-10 py-2">
+									<b>ATM/Bank Transfer</b>
+									<br>
+									<small>Pay from ATM Bersama, Prima or Alto</small>
+								</div>
+
+							</div>
+						</td>
+						<td class="w-50">Hello</td>
+					</tr>
+				</table>
+				<div class="col-md-12 w-100 text-right mt-4">
+					<button class=" btn btn-abbys px-5 py-2 font-weight-bold">PAY NOW</button>
+				</div>
+				<div class="col-md-12 mt-3">
+					<p>(*) Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+						Ipsum is
+						simply dummy text of the printing
+						and typesetting industry.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div data-include="footer-2"> <?php include 'include/footer.php'; ?> </div>
