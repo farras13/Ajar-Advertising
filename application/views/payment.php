982a58b5ba61ad326b3a9d2e7678c1c62684da51
adit <aeka489@gmail.com>
2019-09-12 23:27:15 +0700

    add more payment method

--- application/views/payment.php
+++ application/views/payment.php
@@ -1,9 +1,8 @@
-<div data-include="header"> <?php include 'include/header.php'; ?> </div>
+<div data-include="header"><?php include 'include/header.php'; ?></div>
 
 <div class="col-xl-12 p-0 m-0 w-100">
 	<div class="row w-100 m-0">
 		<div class="col-md-4 m-0 p-0 ">
-
 			<div id="demo" class="carousel slide payment" data-ride="carousel">
 				<!-- Indicators -->
 				<ul class="carousel-indicators ">
@@ -15,19 +14,22 @@
 				<!-- The slideshow -->
 				<div class="carousel-inner h-50">
 					<div class="carousel-item active h-75">
-						<p>Lorem Ipsum adalah contoh teks atau
-							dummy dalam industri percetakan dan
-							penataan huruf atau typesetting</p>
+						<p>
+							Lorem Ipsum adalah contoh teks atau dummy dalam industri
+							percetakan dan penataan huruf atau typesetting
+						</p>
 					</div>
 					<div class="carousel-item  h-75">
-						<p>Lorem Ipsum adalah contoh teks atau
-							dummy dalam industri percetakan dan
-							penataan huruf atau typesetting</p>
+						<p>
+							Lorem Ipsum adalah contoh teks atau dummy dalam industri
+							percetakan dan penataan huruf atau typesetting
+						</p>
 					</div>
 					<div class="carousel-item h-75">
-						<p>Lorem Ipsum adalah contoh teks atau
-							dummy dalam industri percetakan dan
-							penataan huruf atau typesetting</p>
+						<p>
+							Lorem Ipsum adalah contoh teks atau dummy dalam industri
+							percetakan dan penataan huruf atau typesetting
+						</p>
 					</div>
 				</div>
 
@@ -39,7 +41,6 @@
 					<span class="carousel-control-next-icon"></span>
 				</a>
 			</div>
-
 		</div>
 		<div class="col-md-8 pt-5 bg-white">
 			<div class="container px-5">
@@ -48,41 +49,98 @@
 					<p>This Package is Right for You</p>
 				</div>
 
-				<hr>
+				<hr />
 
-
-
-				<table class="table table-bordered">
+				<table id="table-payment" class="table table-bordered">
 					<tr>
 						<td class="w-50">
 							<div class="row">
 								<div class="col-lg-2 py-2 my-auto text-center">
-									<img height="30" width="50"
-										src="https://image.flaticon.com/icons/svg/1899/1899722.svg" alt="">
+									<img
+										height="30"
+										width="50"
+										src="https://image.flaticon.com/icons/svg/1899/1899722.svg"
+										alt="atm"
+									/>
 								</div>
 								<div class="col-lg-10 py-2">
 									<b>ATM/Bank Transfer</b>
-									<br>
+									<br />
 									<small>Pay from ATM Bersama, Prima or Alto</small>
 								</div>
-
 							</div>
 						</td>
-						<td class="w-50">Hello</td>
+						<td class="w-50">
+							<div class="row">
+								<div class="col-lg-2 py-2 my-auto text-center">
+									<img
+										height="20"
+										width="50"
+										src="https://refrez.com/wp-content/uploads/bfi_thumb/logo-gopay-3283zttrcmrz21y5wyx9k1eevtsrj3azdndzvd61i1194tdl6.jpg"
+										alt="atm"
+									/>
+								</div>
+								<div class="col-lg-10 py-2">
+									<b>GO-PAY</b>
+									<br />
+									<small>Pay with your GO-PAY wallet</small>
+								</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="w-50">
+							<div class="row">
+								<div class="col-lg-2 py-2 my-auto text-center">
+									<img
+										height="30"
+										width="50"
+										src="https://image.flaticon.com/icons/svg/1899/1899722.svg"
+										alt="atm"
+									/>
+								</div>
+								<div class="col-lg-10 py-2">
+									<b>ATM/Bank Transfer</b>
+									<br />
+									<small>Pay from ATM Bersama, Prima or Alto</small>
+								</div>
+							</div>
+						</td>
+						<td class="w-50">
+							<div class="row">
+								<div class="col-lg-2 py-2 my-auto text-center">
+									<img
+										height="50"
+										width="50"
+										src="https://2.bp.blogspot.com/-BRpU7CBDb6M/WIBAH-0xwAI/AAAAAAAAAyc/iFEOsdwyb286fmcHjskcV2eptJ8W1YxPACLcB/s1600/Internet%2BBanking%2BBCA%2Bklikbca.png"
+										alt="atm"
+									/>
+								</div>
+								<div class="col-lg-10 py-2">
+									<b>GO-PAY</b>
+									<br />
+									<small>Pay with your GO-PAY wallet</small>
+								</div>
+							</div>
+						</td>
 					</tr>
 				</table>
+
 				<div class="col-md-12 w-100 text-right mt-4">
-					<button class=" btn btn-abbys px-5 py-2 font-weight-bold">PAY NOW</button>
+					<button class=" btn btn-abbys px-5 py-2 font-weight-bold">
+						PAY NOW
+					</button>
 				</div>
 				<div class="col-md-12 mt-3">
-					<p>(*) Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-						Ipsum is
-						simply dummy text of the printing
-						and typesetting industry.</p>
+					<p>
+						(*) Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry. Lorem Ipsum is simply dummy text of the printing and
+						typesetting industry.
+					</p>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 
-<div data-include="footer-2"> <?php include 'include/footer.php'; ?> </div>
+<div data-include="footer-2"><?php include 'include/footer.php'; ?></div>
