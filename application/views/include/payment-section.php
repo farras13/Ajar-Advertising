72ab7e4072ad5cba18838afbc55b96605ccfd6d7
adit <aeka489@gmail.com>
2019-09-13 23:58:54 +0700

    modify payment section

---
+++ application/views/include/payment-section.php
@@ -0,0 +1,72 @@
+<div class="row">
+	<div class="col-md-7 pl-4">
+		<table class="payment-table">
+			<tbody>
+				<tr>
+					<td>
+						<p>Name of Product </p>
+						<p>Price / Unit </p>
+						<p>Total Unit </p>
+						<p>Total Price</p>
+						<p>Discount</p>
+						<p>Tax (10%)</p>
+						<p>Total Payment</p>
+					</td>
+					<td class="pl-5 payment-value font-weight-bold">
+						<p>: Package A </p>
+						<p>: IDR 1.000.000</p>
+						<p>: 1 </p>
+						<p>: IDR 1.000.000</p>
+						<p>: 0</p>
+						<p>: IDR 100.000</p>
+						<p>: IDR 1.100.000</p>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+
+	</div>
+	<div class="col-md-5 payment-radio">
+		<p>Select Your Payment :</p>
+		<p>
+			<span class="input-group-addon">
+				<input name="payment" type="radio">
+				<img class="radio-img"
+					src="https://res.cloudinary.com/dkswpljtg/image/upload/v1531301303/Mastercard-Visa-Limited-Acceptance-Programs_uvpqvf.png">
+			</span>
+		</p>
+		<p>
+			<span class="input-group-addon">
+				<input name="payment" type="radio">
+				<img class="radio-img" src="http://mnctrijaya.com/uploads/news/bank_mandiri.jpg">
+			</span>
+		</p>
+		<p>
+			<span class="input-group-addon">
+				<input name="payment" type="radio">
+				<img class="radio-img" src="http://chandra.my.id/wp-content/uploads/2012/07/logo-bank-bca.jpg">
+			</span>
+		</p>
+		<p>
+			<span class="input-group-addon">
+				<input name="payment" type="radio">
+				<img class="radio-img"
+					src="https://4.bp.blogspot.com/-WPYTKgFutro/WEPx1YEAvXI/AAAAAAAABqg/_X_qPtfzTJYCaBSbzDFGK1z7TpkGAx5LQCLcB/s1600/Cara%2BDaftar%2BE%2BPay%2BBRI%2Bdan%2BKeunggulannya%252Clogin%2Bepay%2Bbri%252Cepay%2Bbri%2Btokopedia%252Ct%2Bbank%2Bbri%252Cepay%2Bmandiri%252Cbiaya%2Be-pay%2Bbri%252Ce%2Bkatalog%2Bbri%252Cmocash%2Bbri%252Ce%2Bcatalog%2Bbri%252C.jpg">
+			</span>
+		</p>
+		<p>
+			<span class="input-group-addon">
+				<input name="payment" type="radio">
+				<img class="radio-img"
+					src="http://3.bp.blogspot.com/-DqRReIuCgcc/UjnSRcKPIgI/AAAAAAAAAJI/66Bwh6-3IIo/s1600/CimbClicks.png">
+			</span>
+		</p>
+		<p>
+			<span class="input-group-addon">
+				<input name="payment" type="radio">
+				<img class="radio-img"
+					src="https://res.cloudinary.com/dkswpljtg/image/upload/v1531301303/Mastercard-Visa-Limited-Acceptance-Programs_uvpqvf.png">
+			</span>
+		</p>
+	</div>
+</div>
