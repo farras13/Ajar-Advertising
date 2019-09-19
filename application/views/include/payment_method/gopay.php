bce8a4e0051fe75ffa2a494d950a5efff04da962
adit <aeka489@gmail.com>
2019-09-19 23:48:09 +0700

    update payment gopay

---
+++ application/views/include/payment_method/gopay.php
@@ -0,0 +1,44 @@
+<div class="container">
+	<div class="text-center">
+		<h3>LET’S FINISH POWERING YOU UP !</h3>
+		<p>This Package is Right for You</p>
+	</div>
+	<hr>
+
+	<div>
+
+		<li class="list-group-item border-0 fz-2"> How to Pay?
+			<img class="float-right" height="30" width="auto"
+				src="https://refrez.com/wp-content/uploads/bfi_thumb/logo-gopay-3283zttrcmrz21y5wyx9k1eevtsrj3azdndzvd61i1194tdl6.jpg"
+				alt="atm" />
+		</li>
+		<div class="alert alert-warning px-lg-5">
+			Please complete your <b>GO-PAY</b> payment via <b>GO-JEK</b> app
+		</div>
+		<div style="max-height: 250px; min-height: 100px;" class="border overflow-auto">
+
+			<li class="list-group-item border-left-0 border-right-0 fz-2 px-5">
+				1. CLik Pay Now With GO-PAY
+			</li>
+			<li class="list-group-item border-left-0 border-right-0 fz-2 px-5">
+				2. Open GO-JEK app on your phone
+			</li>
+			<li class="list-group-item border-left-0 border-right-0 fz-2 px-5">
+				1. CLik Pay Now With GO-PAY
+			</li>
+			<li class="list-group-item border-left-0 border-right-0 fz-2 px-5">
+				2. Open GO-JEK app on your phone
+			</li>
+			<li class="list-group-item border-left-0 border-right-0 fz-2 px-5">
+				1. CLik Pay Now With GO-PAY
+			</li>
+			<li class="list-group-item border-left-0 border-right-0 fz-2 px-5">
+				2. Open GO-JEK app on your phone
+			</li>
+
+
+		</div>
+		<hr>
+
+	</div>
+</div>
--- application/views/payment.php
+++ application/views/payment.php
@@ -43,7 +43,7 @@
 			</div>
 		</div>
 		<div class="col-md-8 pt-5 bg-white">
-		<?php include 'include/payment_method/credit-card.php'; ?>
+		<?php include 'include/payment_method/gopay.php'; ?>
 		</div>
 	</div>
 </div>
