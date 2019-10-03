e5493652108908abfbdd7cd43cb2c172df8167fa
adit <aeka489@gmail.com>
2019-10-03 22:55:40 +0700

    change header

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -1,33 +1,64 @@
- <div id="nav" class="mx-4 blog-header navbar-fixed-top">
+ <!-- <div id="nav" class="mx-4 blog-header navbar-fixed-top">
+ 	<header class=" py-2 px-5">
+ 		<div class="row flex-nowrap justify-content-between align-items-center">
+ 			<div class="col-4 pt-1">
+ 			</div>
+ 			<div class="col-4 text-center">
+ 				<a href="<?php echo site_url() ?>/Home"><img class="logo-header"
+ 						src="<?php echo base_url() ?>/assets/img/logo-adv.svg" alt="logo"></a>
+ 			</div>
+ 			<div class="col-4 d-flex justify-content-end align-items-center">
+ 				<a class="text-blue text-decoration-none mr-4" href="<?php echo site_url() ?>/Contact">
+ 					<i class="fa fa-phone" aria-hidden="true"></i>
+ 					Contact Us
+ 				</a>
+ 				<a class="btn btn-grad-warning btn-rounded px-4 fz-1 mr-3" href="<?php echo site_url() ?>/Login">
+ 					Start Now</a>
+ 				<a class="text-blue border-left pl-3 text-decoration-none mr-4"
+ 					href="<?php echo site_url() ?>/Contact">
+ 					<i class="fa fa-globe" aria-hidden="true"></i>
+ 				</a>
+ 			</div>
+ 		</div>
+ 	</header>
 
-                <header class=" py-2 px-5">
-                    <div class="row flex-nowrap justify-content-between align-items-center">
-                        <div class="col-4 pt-1">
-                        </div>
-                        <div class="col-4 text-center">
-                            <a href="<?php echo site_url() ?>/Home"><img class="logo-header" src="<?php echo base_url() ?>/assets/img/logo-adv.svg" alt="logo"></a>
-                        </div>
-                        <div class="col-4 d-flex justify-content-end align-items-center">
-                            <a class="text-blue text-decoration-none mr-4" href="<?php echo site_url() ?>/Contact">
-                                <i class="fa fa-phone" aria-hidden="true"></i>
-                                Contact Us
-                            </a>
-                            <a class="btn btn-grad-warning btn-rounded px-4 fz-1 mr-3" href="<?php echo site_url() ?>/Login">
-                                Start Now</a>
-                            <a class="text-blue border-left pl-3 text-decoration-none mr-4" href="<?php echo site_url() ?>/Contact">
-                                <i class="fa fa-globe" aria-hidden="true"></i>
-                            </a>
-                        </div>
-                    </div>
-                </header>
+ 	<div class="nav-scroller py-1 mb-2 border-top border-2">
+ 		<nav class="nav d-flex justify-content-center ">
+ 			<a class="px-4 py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/paket">Paket
+ 				Artikel</a>
+ 			<a class="px-4 py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/paket">Paket
+ 				Interview</a>
+ 			<a class="px-4 py-1 text-muted text-decoration-none"
+ 				href="<?php echo site_url() ?>/Home/Payment">Afiliasi</a>
+ 			<a class="px-4  py-1 text-muted text-decoration-none"
+ 				href="<?php echo site_url() ?>/Home/Privacy">Promo</a>
+ 			<a class="px-4  py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/Home/Faq">Blog</a>
+ 		</nav>
+ 	</div>
+ </div> -->
 
-                <div class="nav-scroller py-1 mb-2 border-top border-2">
-                    <nav class="nav d-flex justify-content-center ">
-                        <a class="px-4 py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/paket">Paket Artikel</a>
-                        <a class="px-4 py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/paket">Paket Interview</a>
-                        <a class="px-4 py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/Home/Payment">Afiliasi</a>
-                        <a class="px-4  py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/Home/Privacy">Promo</a>
-                        <a class="px-4  py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/Home/Faq">Blog</a>
-                    </nav>
-                </div>
-            </div>
+ <nav class="navbar navbar-expand-lg blog-header bg-light py-3">
+ 	<!-- <a class="navbar-brand" href="#">Navbar</a> -->
+ 	<div class="mx-auto navbar-brand ">
+ 		<a href="<?php echo site_url() ?>/Home"><img class="logo-header"
+ 				src="<?php echo base_url() ?>/assets/img/logo-adv.svg" alt="logo"></a>
+ 	</div>
+ 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+ 		aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+ 		<span class="navbar-toggler-icon"></span>
+ 	</button>
+
+ 	<div class="collapse navbar-collapse float-right w-auto" id="navbarSupportedContent">
+ 		<div class="w-auto mr-0 ml-auto">
+ 			<a class="text-blue text-decoration-none mr-4" href="<?php echo site_url() ?>/Contact">
+ 				<i class="fa fa-phone" aria-hidden="true"></i>
+ 				Contact Us
+ 			</a>
+ 			<a class="btn btn-grad-warning btn-rounded px-4 fz-1 mr-3" href="<?php echo site_url() ?>/Login">
+ 				Start Now</a>
+ 			<a class="text-blue border-left pl-3 text-decoration-none mr-4" href="<?php echo site_url() ?>/Contact">
+ 				<i class="fa fa-globe" aria-hidden="true"></i>
+ 			</a>
+ 		</div>
+ 	</div>
+ </nav>
