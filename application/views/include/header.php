21faae98d522e9a7cd6351447afc84068c6863d5
adit <aeka489@gmail.com>
2019-10-03 23:29:00 +0700

    update header

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -37,10 +37,10 @@
  	</div>
  </div> -->
 
- <nav class="navbar navbar-expand-lg blog-header bg-light py-3">
+ <nav class="navbar navbar-expand-lg navbar-light blog-header py-3">
  	<!-- <a class="navbar-brand" href="#">Navbar</a> -->
- 	<div class="mx-auto navbar-brand ">
- 		<a href="<?php echo site_url() ?>/Home"><img class="logo-header"
+ 	<div class="mx-auto mt-2 navbar-brand ">
+ 		<a class="py-4" href="<?php echo site_url() ?>/Home"><img class="logo-header"
  				src="<?php echo base_url() ?>/assets/img/logo-adv.svg" alt="logo"></a>
  	</div>
  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -48,7 +48,7 @@
  		<span class="navbar-toggler-icon"></span>
  	</button>
 
- 	<div class="collapse navbar-collapse float-right w-auto" id="navbarSupportedContent">
+ 	<div class="collapse navbar-collapse my-2 float-right w-auto" id="navbarSupportedContent">
  		<div class="w-auto mr-0 ml-auto">
  			<a class="text-blue text-decoration-none mr-4" href="<?php echo site_url() ?>/Contact">
  				<i class="fa fa-phone" aria-hidden="true"></i>
@@ -61,4 +61,38 @@
  			</a>
  		</div>
  	</div>
+
  </nav>
+ <div id="myHeader" class="nav-scroller py-1 mb-2 border-top blog-header border-2">
+ 	<nav class="nav d-flex justify-content-center ">
+ 		<a class="px-4 py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/paket">Paket
+ 			Artikel</a>
+ 		<a class="px-4 py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/paket">Paket
+ 			Interview</a>
+ 		<a class="px-4 py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/Home/Payment">Afiliasi</a>
+ 		<a class="px-4  py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/Home/Privacy">Promo</a>
+ 		<a class="px-4  py-1 text-muted text-decoration-none" href="<?php echo site_url() ?>/Home/Faq">Blog</a>
+ 	</nav>
+ </div>
+
+ <script>
+ 	window.onscroll = function () {
+ 		myFunction()
+ 	};
+
+ 	var header = document.getElementById("myHeader");
+ 	var sticky = header.offsetTop;
+
+ 	function myFunction() {
+ 		if (window.pageYOffset > sticky) {
+ 			header.classList.add("sticky");
+ 			$(header).addClass("bg-white zindex-foward")
+
+ 		} else {
+ 			header.classList.remove("sticky");
+ 			$(header).removeClass("bg-white")
+
+ 		}
+ 	}
+
+ </script>
